24292b47b17f10ea7f37ea8c58e96605b3482647
subrata410 <subrata.rooy2022@gmail.com>
2023-01-07 23:46:22 +0600

    JavaScript Class Nine

---
+++ script.js
@@ -0,0 +1,47 @@
+// (1) getElementById method
+// const id1 = document.getElementById("id1");
+// // console.log(id1);
+// console.log(id1.innerHTML);
+// id1.innerHTML = "JavaScript Text"
+// document.getElementById("id1").innerHTML = "New JavaScript Text";
+
+// (2) getElementByTagName Method
+// const tag1 = document.getElementsByTagName("h1")[1];
+// console.log(tag1.innerHTML);
+
+
+// (3) getElementByClassName
+// const class1 = document.getElementsByClassName("class1")[0];
+// console.log(class1.innerText);
+// class1.innerText = "JavaScript Text"
+
+// (4) DOM Element style
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+// id1.style.color = "pink";
+// id1.style.backgroundColor = "green";
+// id1.style.fontSize = "60px";
+
+// (5) DOM Element Attributes
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+
+// console.log(id1.attributes);
+// console.log(id1.attributes.length)
+
+// (6) getAttribute
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+// console.log(id1.getAttribute("class"));
+// console.log(id1.getAttribute("id"));
+
+
+// // (7) removeAttribute
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+// id1.removeAttribute("class")
+
+// (8) setAttribute
+// const id2 = document.getElementById("id2");
+// console.log(id2.innerText);
+// id2.setAttribute("class", "class2")
